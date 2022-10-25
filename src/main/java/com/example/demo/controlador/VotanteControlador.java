84cf95d9b092b70532dd1ae784904a74bfcbbacc
DiegoOrtiz08 <diegocamiloortizp@gmail.com>
2022-10-25 12:13:29 -0500

    Agregar, Editar y Borrar
    Botones Agregar, Editar y Borrar funcionando

--- src/main/java/com/example/demo/controlador/VotanteControlador.java
+++ src/main/java/com/example/demo/controlador/VotanteControlador.java
@@ -2,12 +2,14 @@ package com.example.demo.controlador;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entidades.Votante;
@@ -31,6 +33,22 @@ public class VotanteControlador {
 		return "/PetroCSS";
 	}
 
+	@GetMapping("/hernandez")
+	public String rodolfo() {
+		return "/Hernandez";
+	}
+
+	
+	@GetMapping("/federico")
+	public String federico() {
+		return "/Federico";
+	}
+	
+	@GetMapping("/fajardo")
+	public String fajardo() {
+		return "/fajardo";
+	}
+
 	@GetMapping("/votante")
 	public String votante(Model model) {
 		try {
@@ -52,23 +70,19 @@ public class VotanteControlador {
 	public String addVotante(@Validated Votante votante) {
 		System.out.println("Votante-->"+votante.toString());
 		votanteService.save(votante);
-		return "redirect:/votapp";
-	}
-	
-	@GetMapping("/hernandez")
-	public String rodolfo() {
-		return "/Hernandez";
-		
+		return "redirect:/votante";
 	}
 
-	
-	@GetMapping("/federico")
-	public String federico() {
-		return "/Federico";
+	@GetMapping("/editarVotante/{id}")
+	public String Editar(@PathVariable int id, Model model) {
+		Optional<Votante> votante=votanteService.findById(id);
+		model.addAttribute("votante", votante.get());
+		return "/edit";
 	}
-	
-	@GetMapping("/fajardo")
-	public String fajardo() {
-		return "/fajardo";
-	}
+
+	@GetMapping("/eliminarVotante/{id}")
+	public String eliminar (@PathVariable int id) {
+		votanteService.deleteById(id);
+		return "redirect:/votante";
+	}	
 }
